81007af43835bdbbb2f714f6310231d8a8659c02
CHIRAG MISHRA <92207681+chiragmishra03@users.noreply.github.com>
2024-01-30 02:01:11 +0530

    Updated Solution - GfG to GitHub

--- Medium/LCS of three strings/lcs-of-three-strings.cpp
+++ Medium/LCS of three strings/lcs-of-three-strings.cpp
@@ -2,7 +2,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int LCSof3 (string A, string B, string C, int n1, int n2, int n3);
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+
+        int LCSof3 (string A, string B, string C, int n1, int n2, int n3)
+        {
+            int dp[n1+1][n2+1][n3+1];
+            memset(dp, 0, sizeof(dp));
+            for(int i=1; i<=n1; i++)
+            {
+                for(int j=1; j<=n2; j++)
+                {
+                    for(int k=1; k<=n3; k++)
+                    {
+                        if (A[i-1] == B[j-1] && B[j-1] == C[k-1])
+                        {
+                            dp[i][j][k]=dp[i-1][j-1][k-1]+1;
+                        }
+                        else
+                        {
+                            dp[i][j][k]=max(max(dp[i-1][j][k], dp[i][j-1][k]),dp[i][j][k-1]);
+                        }
+                    }
+                }
+            }
+            return dp[n1][n2][n3];
+        }
+};
+
+//{ Driver Code Starts.
+
 int main()
 {
     int t; cin >> t;
@@ -10,37 +44,10 @@ int main()
     {
         int n1, n2, n3; cin >> n1 >> n2 >> n3;
         string A, B, C; cin >> A >> B >> C;
-        cout << LCSof3 (A, B, C, n1, n2, n3) << endl;
+        Solution obj;
+        cout << obj.LCSof3 (A, B, C, n1, n2, n3) << endl;
     }
 }
 // Contributed By: Pranay Bansal
 
 // } Driver Code Ends
-
-
-int lcs(string s1, string s2, string s3) {
-    int n = s1.size();
-    int m = s2.size();
-    int k = s3.size();
-    vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(m + 1, vector<int>(k + 1, 0)));
-
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= m; ++j) {
-            for (int l = 1; l <= k; ++l) {
-                if (s1[i - 1] == s2[j - 1] && s1[i - 1] == s3[l - 1]) {
-                    dp[i][j][l] = 1 + dp[i - 1][j - 1][l - 1];
-                } else {
-                    dp[i][j][l] = max(dp[i - 1][j][l], max(dp[i][j - 1][l], dp[i][j][l - 1]));
-                }
-            }
-        }
-    }
-    return dp[n][m][k];
-}
-
-
-int LCSof3 (string A, string B, string C, int n1, int n2, int n3)
-{
-   return lcs(A,B,C);
-   
-}
